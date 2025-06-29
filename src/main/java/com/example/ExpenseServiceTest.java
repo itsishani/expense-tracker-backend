d9de3521458b1f3113d0857f5c150b4cb5e89a1d
itsishani <121381877+itsishani@users.noreply.github.com>
2025-06-30 01:16:45 +0530

    Create ExpenseServiceTest.java

---
+++ src/main/java/com/example/ExpenseServiceTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExpenseServiceTest {
+
+    @Test
+    void addTwoNumbers() {
+        assertEquals(8, 5 + 3);
+    }
+}
